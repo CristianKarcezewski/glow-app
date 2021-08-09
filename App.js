fd9eb23516c07ce99556d2f8cc3bfc78d876f6b5
develcode12 <cristian.karcezewski@itlean.com.br>
2021-08-09 08:53:48 -0300

    image touchable

--- App.js
+++ App.js
@@ -1,19 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello world!</Text>
-      {/* <Image source={require('./assets/icon.png')} /> */}
-      <Image
-        source={{
-          width: 200,
-          heigth: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+      <TouchableWithoutFeedback onPress={() => console.log('Image pressed!')}>
+        <Image source={{uri: 'https://picsum.photos/200/300'}}
+          style={{width:200, height:300}}
+        />
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 }
